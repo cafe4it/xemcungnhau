3375e3c15f1815fbaf696814b9adc724627e28f2
nxpeacock <nguyenxuancong@gmail.com>
2015-05-10 00:08:37 +0700

    Khởi tạo giao diện

--- server/publish.js
+++ server/publish.js
@@ -5,18 +5,3 @@ Meteor.publish(null, function (){
 Meteor.publish("users_admin_list",function(){
     return Meteor.users.find();
 })
-
-Meteor.publishComposite('challenge_by_id',function(id){
-    return {
-        find : function(){
-            return Challenges.find({_id: id})
-        },
-        children : [
-            {
-                find : function(challenge){
-                    return Meteor.users.find({_id : challenge.author})
-                }
-            }
-        ]
-    }
-})
--- server/security.js
+++
@@ -1,2 +0,0 @@
-Challenges.permit(['insert', 'update', 'remove']).ifLoggedIn().apply();
-
