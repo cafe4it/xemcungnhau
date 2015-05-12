bb6f55fab06e7f299a6a996e7d536a45e710fa18
nxpeacock <nguyenxuancong@gmail.com>
2015-05-12 22:58:45 +0700

    mệt quá

--- client/views/channel/channel_create.js
+++ client/views/channel/channel_create.js
@@ -4,6 +4,14 @@ Template.channel_create.helpers({
 
 AutoForm.hooks({
     insertChannelForm : {
+        onSubmit : function(doc){
+            if(doc){
+                _.extend(doc,{userId : Meteor.userId()})
+                console.log(doc);
+                this.done();
+                return true;
+            }
+        },
         onSuccess : function(type, result){
             if(result){
                 var def = _.template('/channel/<%=userId%>'),
