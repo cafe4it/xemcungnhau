bb1cd45bbf2259372d87cab1adcfb5c2593a7847
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2015-05-12 15:19:16 +0700

    channel of friends

--- server/publish.js
+++ server/publish.js
@@ -2,6 +2,10 @@ Meteor.publish(null, function (){
     return Meteor.roles.find({})
 })
 
+Meteor.publish(null,function(){
+    return Meteor.users.find();
+})
+
 Meteor.publish("users_admin_list",function(){
     return Meteor.users.find();
 })
